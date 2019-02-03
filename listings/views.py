b000bcee39636b7b9faeea70a0f1c55c0b9d9d49
Niaz Ahmed <itsniaz@gmail.com>
2019-02-03 23:33:22 +0600

    Made pages dynamic

--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Listing
 
 
@@ -8,14 +8,22 @@ def listings(request):
     listingsList = Listing.objects.all()
 
 
+
     context = {
         'listings' : listingsList
     }
 
     return render(request, "pages/listings.html", context)
 
-def listing(request):
-    return render(request,"pages/listing.html")
+def listing(request, listing_id):
+
+    listing = get_object_or_404(Listing, pk=listing_id)
+
+    context = {
+        'listing' : listing
+    }
+
+    return render(request,"pages/listing.html", context)
 
 def search(request):
     return render(request, "pages/search.html")
--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from listings.models import Listing
 
 # Create your views here.
 def index(request):
-    return render(request, "pages/index.html")
+
+    listings = Listing.objects.all()
+ 
+    context = {
+        'listings' : listings
+    }
+
+    return render(request, "pages/index.html", context)
 
 
 def about(request):
